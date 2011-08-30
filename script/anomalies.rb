2d4a2bab1d30349197b7ab6325fc871225b1cdcb
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2011-08-30 12:11:53 -0300

    mailmap.rb merged into anomalies.rb

--- script/anomalies.rb
+++ script/anomalies.rb
@@ -15,15 +15,44 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'uri'
+require 'optparse'
 require 'yaml'
 
+opts = ARGV.getopts('', 'no-fix', 'plain', 'mailmap')
 config = YAML.load_file(ARGV.first)
 projects = YAML.load_file(config[:list][:file])
 
 perlexpr = 'print $_ unless Mail::RFC822::Address::valid($_)'
 check = "perl -I#{File.dirname(__FILE__)} -MAddress -ne '#{perlexpr}'"
+git = ["git --git-dir ", " log --pretty='%aE%x0A%cE'"]
 projects.each_index do |i| project = projects[i]
   STDERR.printf "%5d/%d - %s\n", i + 1, projects.size, project[:path]
-  system "git --git-dir #{project[:dir]} log --pretty='%aE%x0A%cE' | #{check}"
+  IO.popen "#{git.join(project[:dir])} | sort | uniq | #{check}" do |io|
+    if opts['no-fix']
+      puts io.read
+    else
+      io.read.each_line do |line|
+        line.strip!
+        email = URI.unescape(line)
+        email.gsub!(/DOT/, '.')
+        email.gsub!(/AT/, '@')
+        email.downcase!
+        email.gsub!(/[^-._@a-z0-9]/, ' ')
+        email.squeeze!(' ')
+        email.gsub!(/[ _.-]+at[-._ ]+/, '@')
+        email.gsub!(/[ _.-]+dot[-._ ]+/, '.')
+        email.gsub!(/^[ _.-]+|[-._ ]+$/, '')
+        email.sub!(' ', '@') if email.count('@') == 0
+        email.gsub!(' ', '.')
+        email.squeeze!('.')
+        email = email.split('@').last if email.include? 'no.author.'
+        email = email.split('@').first if email.include? '.none'
+        (puts "<#{email}> <#{line}>"; next) if opts['mailmap']
+        (puts email; next) if opts['plain']
+        puts [[line, email]].to_yaml[5..-1]
+      end
+    end
+  end
 end
 
