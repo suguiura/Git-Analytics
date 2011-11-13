19f23d83c2a968ca70c09e9fbbb470ac347ae472
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2011-11-13 20:54:55 -0200

    Passing extra data, removing create_person and removing recodification

--- script/git.rb
+++ script/git.rb
@@ -8,9 +8,9 @@ module GitAnalytics
       m = IO.popen(git).read.to_i
     end
 
-    def self.log(gitdir, range='')
+    def self.log(gitdir, range='', extra={})
       git = "git --git-dir #{gitdir} log #{range} -z --decorate --stat --pretty=raw"
-      IO.popen(git){|io| io.each("\0"){|line| yield(parse(line))}}
+      IO.popen(git){|io| io.each("\0"){|line| yield(parse(line, extra))}}
     end
 
     private
@@ -22,31 +22,27 @@ module GitAnalytics
     @re_message = /^    (.*)$/
     @re_commit = /^commit (\S+) ?(.*)$/
 
-    def self.create_person(name, email)
-      {:name => name, :email => email}
-    end
-
     def self.create_date(secs, offset)
       Time.at(secs.to_i).getlocal(offset.insert(3, ':'))
     end
 
     def self.parse_signatures(line)
       line.scan(@re_signatures).map do |key, name, email|
-        {:name => key, :person => create_person(name, email)}
+        {:name => key, :person => {:name => name, :email => email || name}}
       end
     end
 
     def self.parse_modifications(line)
-      line.scan(@re_modifications).map{|p, c| {:path => p.strip, :linechanges => c}}
+      line.scan(@re_modifications).map{|p, c| {:path => p.strip, :linechanges => c.to_i}}
     end
 
     def self.parse_person(header, line)
       name, email, secs, offset = @re_person[header].match(line).captures
-      {:date => create_date(secs, offset)}.update(create_person(name, email))
+      {:date => create_date(secs, offset), :name => name, :email => email}
     end
 
-    def self.parse(line)
-      line = line.encode(Encoding::UTF_8, Encoding::ISO8859_1).strip
+    def self.parse(line, extra)
+      line = line.strip
 
       sha1, tag = @re_commit.match(line).captures
       message = line.scan(@re_message).join("\n").strip
@@ -59,7 +55,7 @@ module GitAnalytics
         :committer      => parse_person('committer', line),
         :signatures     => parse_signatures(line),
         :modifications  => parse_modifications(line)
-      }
+      }.update(extra)
     end
   end
 end
