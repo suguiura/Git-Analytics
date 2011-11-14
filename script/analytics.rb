1cb008b816205b8b212ab4dbc46d79db37acd4a9
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2011-11-13 22:22:46 -0200

    Refactorings but in test phase.

--- script/analytics.rb
+++ script/analytics.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 
-require 'uri'
-
 $: << File.dirname(__FILE__)
 require 'config'
 require 'git'
 require 'db'
 require 'schema'
+require 'gcsv'
+require 'uri'
+
+$l.info "Start"
 
 gtld = '(%s)' % $config[:gtlds].join('|')
 cctld = '(%s)' % $config[:cctlds].join('|')
@@ -20,43 +22,45 @@ def get_sld(homepage)
 end
 
 def associate_companies()
-  $l.info "Associating companies"
+  n = Company.count
+  $l.info "Associating companies (#{n})"
   Company.find_each do |company| domain = get_sld(company.homepage)
+    n = step_log(n, 1000, '', " companies left")
     condition = {:conditions => ['email like ?', "%@#{domain}"]}
     company.authors = Author.find(:all, condition) unless domain.nil?
   end
-  $l.info "Done"
 end
 
 def process_project(data)
-  m = GitAnalytics::Git.count(data[:dir], data[:range])
-
-  $l.info "total: %d" % m
-  GitAnalytics::Git.log(data[:dir], data[:range]) do |log|
-    m = step_log(m, 1000, 'commits: ')
-    log[:origin] = data[:origin]
-    log[:name] = data[:name]
-    log[:description] = data[:description]
-    GitAnalytics::DB.store(log)
-    GitAnalytics::CSV.store(log)
+  dir, range = data[:dir], data[:range]
+  extra = {:origin      => data[:origin],
+           :project     => data[:name],
+           :description => data[:description]}
+  n = GitAnalytics::Git.count(dir, range)
+  $l.info "total: %d" % n
+  GitAnalytics::Git.log(dir, range, extra) do |log|
+    n = step_log(n, 1000, 'commits: ')
+#    GitAnalytics::DB.store(log)
+    GitAnalytics::GCSV.store(log)
   end
+  $l.info "done"
 end
 
-def load_origin(config, project)
-  origin = config[:origin]
-  regex, default = Regexp.new(origin[:regexp]), origin[:default]
-  regex.match(project).captures.first rescue default
-end
-
-each_server_config "Updating database for " do |server, config|
-  GitAnalytics::Schema.create_tables
-  GitAnalytics::Schema.add_indexes
-  n = $projects[server].size rescue next
-  $projects[server].each do |project, data|
+def process_server(server, config, projects)
+  n = projects.size
+  projects.each do |project, data|
     n = step_log(n, 1, '', " - #{project}")
-    data[:origin] = load_origin(config, project)
+    GitAnalytics::GCSV.open(config[:data][:csv])
     process_project(data)
   end
+end
+
+each_server_config "Processing " do |server, config, projects|
+#  GitAnalytics::Schema.create_tables
+#  GitAnalytics::Schema.remove_indexes
+#  GitAnalytics::Schema.add_indexes
+  process_server(server, config, projects)
 #  associate_companies
 end
 
+$l.info "Finish!"
