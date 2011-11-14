c08ab1fe7eb1d5cdb73f6d8e7e8c37a910a70bb2
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2011-11-14 03:03:36 -0200

    Passing server data

--- script/analytics.rb
+++ script/analytics.rb
@@ -12,33 +12,30 @@ $l.info "Start"
 
 def process_project(data)
   dir, range = data[:dir], data[:range]
-  extra = {:origin      => data[:origin],
+  extra = {:server      => data[:server],
+           :origin      => data[:origin],
            :project     => data[:name],
            :description => data[:description]}
   n = GitAnalytics::Git.count(dir, range)
   $l.info "total: %d" % n
   GitAnalytics::Git.log(dir, range, extra) do |log|
     n = step_log(n, 1000, 'commits: ')
-#    GitAnalytics::DB.store(log)
+    GitAnalytics::DB.store(log)
     GitAnalytics::GCSV.store(log)
   end
   $l.info "done"
 end
 
-def process_server(server, config, projects)
+GitAnalytics::Schema.create_tables
+#GitAnalytics::Schema.remove_indexes
+GitAnalytics::Schema.add_indexes
+each_server_config "Processing " do |server, config, projects|
   n = projects.size
   projects.each do |project, data|
     n = step_log(n, 1, '', " - #{project}")
     GitAnalytics::GCSV.open(config[:data][:csv])
     process_project(data)
   end
-end
-
-each_server_config "Processing " do |server, config, projects|
-#  GitAnalytics::Schema.create_tables
-#  GitAnalytics::Schema.remove_indexes
-#  GitAnalytics::Schema.add_indexes
-  process_server(server, config, projects)
 end
 
 $l.info "Finish!"
