8c1036b5312e8daac37a65b4e3aaec7a55c13dbb
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2011-12-24 17:07:55 -0200

    csv-2 created

---
+++ csv-2.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+
+require 'logger'
+require 'active_record'
+
+load 'lib/db.rb'
+
+def relate_companies(company, other_company)
+  a, b = company, other_company
+  a_permalink = a.permalink || raise
+  b_permalink = b.permalink || raise
+  competition = a.competitors.include?(b) rescue false
+  counter = a.similarities.find_or_create_by_other_company(b).counter rescue 0
+  similarity = counter.to_f / a.tags.size rescue 0.0
+  [a_permalink, b_permalink, competition, similarity]
+end
+
+def process_companies(project, companies)
+  companies.each do |path, array|
+    array.uniq.permutation(2).each do |company, other_company|
+      puts [
+        path,
+        project.server.name,
+        project.origin,
+        project.name,
+        relate_companies(company, other_company)
+      ].join("\t")
+    end
+  end
+end
+
+def process
+  puts %w(file server origin project org1 org2 competition tag_similarity).join("\t")
+  $l.info 'projects: %d' % (n = GitAnalytics::DB::Project.count)
+  GitAnalytics::DB::Project.find_each do |project|
+    $l.info '%d: %s' % [n -= 1, project.name]
+#    next if n > 645
+    companies = Hash.new{|h, v| h[v] = []}
+    project.commits.each do |commit|
+      company = commit.author.domain.company
+      next if company.nil? or company.permalink.nil?
+      commit.modifications.each do |modification|
+        companies[modification.metafile.path] << company
+      end
+    end
+    $l.info 'printing'
+    process_companies project, companies
+  end
+end
+
+def prepare
+  require 'yaml'
+  config = YAML::load_file 'config.yaml'
+  GitAnalytics::DB.connect config[:db][:commits], config[:db][:crunchbase]
+end
+
+$l = Logger.new STDERR
+$l.formatter = Logger::Formatter.new
+
+$l.info 'Start'
+prepare
+process
+$l.info 'Finish'
+
