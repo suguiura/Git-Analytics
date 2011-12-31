639761194ef2b7f0572ed7dbea3fd16bc8cb6232
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2011-12-31 03:33:02 -0200

    Migration from Person to Email.

--- migrate.rb
+++ migrate.rb
@@ -12,33 +12,18 @@ $l.info 'Start'
 $config = YAML::load_file 'config.yaml'
 
 ActiveRecord::Base.establish_connection $config[:db][:commits]
-=begin
 ActiveRecord::Schema.define do
 # http://api.rubyonrails.org/classes/ActiveRecord/Migration.html
-  add_column :modifications, :metafile_id, :integer
-  add_index :modifications, :metafile_id
-  create_table   :metafiles do |t|
-    t.string     :path, :default => '', :limit => 64
-  end
-  add_index :metafiles, :path
-#  remove_column :modifications, :path
-end
-=end
-
-class Metafile < ActiveRecord::Base
-  has_many :modifications
-end
-
-class Modification < ActiveRecord::Base
-  belongs_to :metafile
-end
-
-n = Modification.count
-$l.info "total: %d" % n
-Modification.find_each do |modification|
-  modification.metafile = Metafile.find_or_create_by_path modification.path
-  modification.save
-  $l.info "%d left" % [n] if (n -= 1) % 10000 == 0
+  rename_table :domains, :emails
+  rename_column :emails, :address, :raw
+  rename_column :people, :domain_id, :email_id
+  add_column :emails, :user, :default => '', :limit => 32
+  add_index :emails, :raw
+  add_index :emails, :orgdomain
+  add_index :people, :email_id
+  remove_index :people, :domain_id
+  remove_index :people, :email
+  remove_index :emails, :name
 end
 
 $l.info 'Done'
